768577f23a3795f0a5d398665775a97e5268298e
Thiago <thia.mdossantos@gmail.com>
2018-05-21 17:09:25 -0300

    feat: add generate function

--- index.d.ts
+++ index.d.ts
@@ -1,7 +1,20 @@
 declare module 'cnab-file' {
-  export = cnabFile
+    export = cnabFile;
 }
 
 declare namespace cnabFile {
-
+    interface IEntry {
+        start: number;
+        end: number;
+        value: string;
+    }
+    type Line = IEntry[];
+    interface IRemittance {
+        lineSize: number;
+        eol: string;
+        eof: string;
+        lines: Line[];
+    }
+    type RemittanceFile = string;
+    function generate(remittance: IRemittance): RemittanceFile;
 }
