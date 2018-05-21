94d469aa2925634a9b64c1b664e4b501fcc5f4e5
Thiago Santos <thia.mdossantos@gmail.com>
2018-05-21 19:26:32 -0300

    feat: add generate function (#2)

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,44 @@
-export function parse(params: string): string {
-    return params;
+export interface IEntry {
+    start: number;
+    end: number;
+    value: string;
 }
 
-export function generate(params: string): string {
-    return params;
+export type Line = IEntry[];
+
+export interface IRemittance {
+    lineSize: number;
+    eol: string;
+    eof: string;
+    lines: Line[];
+}
+
+export type RemittanceFile = string;
+
+function buildLine(lineSize: number): (line: Line) => string {
+    return (line: Line): string => {
+        const initialValue: string[] = Array(lineSize).fill(' ');
+
+        return line.reduce(replaceValues, initialValue).join('');
+    };
+}
+
+function replaceValues(values: string[], item: IEntry): string[] {
+    const chars: string[] = item.value
+        .substr(0, item.end - item.start + 1)
+        .split('');
+
+    chars.forEach((char: string, index: number) => {
+        values[item.start + index - 1] = char;
+    });
+
+    return values;
+}
+
+export function generate(remittance: IRemittance): RemittanceFile {
+    return (
+        remittance.lines
+            .map(buildLine(remittance.lineSize))
+            .join(remittance.eol) + remittance.eof
+    );
 }
--- test/index.spec.ts
+++ test/index.spec.ts
@@ -1,7 +1,42 @@
-import {parse} from '../src/index';
+import {generate} from '../src/index';
 
-describe('cnab-file', () => {
-    it('should pass', () => {
-        expect(parse('hey')).toEqual('hey');
+describe('generate', () => {
+    it('should generate an remmitt', () => {
+        const input = {
+            lineSize: 10,
+            eol: '\n',
+            eof: '\n\n',
+            lines: [
+                [
+                    {
+                        start: 1,
+                        end: 2,
+                        value: '1'
+                    },
+                    {
+                        start: 5,
+                        end: 7,
+                        value: 'SOMETHING'
+                    }
+                ],
+                [
+                    {
+                        start: 3,
+                        end: 5,
+                        value: 'P'
+                    },
+                    {
+                        start: 8,
+                        end: 10,
+                        value: 'SOMETHING'
+                    }
+                ]
+            ]
+        };
+
+        const actual = generate(input);
+        const expected = '1   SOM   \n  P    SOM\n\n';
+
+        expect(actual).toBe(expected);
     });
 });
